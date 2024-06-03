312f980be7baa2cddf0e8bc1a9711b1a4427a408
KingMusawir <kingola23@gmail.com>
2024-06-03 05:46:42 -0700

    first comit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Faste Pizza
+      </Link>
+      <SearchOrder />
+      <Username />
+    </header>
+  );
+}
+
+export default Header;
